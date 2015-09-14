7aad2452b850724f5070c4721b7eb4415d727f2e
Pushpal Sidhu <psidhu@gateworks.com>
2015-09-14 12:42:48 -0700

    scripts: download.pl: Add Gateworks mirror
    Signed-off-by: Pushpal Sidhu <psidhu@gateworks.com>

--- scripts/download.pl
+++ scripts/download.pl
@@ -221,6 +221,7 @@ foreach my $mirror (@ARGV) {
 #push @mirrors, 'http://mirror1.openwrt.org';
 push @mirrors, 'http://mirror2.openwrt.org/sources';
 push @mirrors, 'http://downloads.openwrt.org/sources';
+push @mirrors, 'http://dev.gateworks.com/sources';
 
 while (!$ok) {
 	my $mirror = shift @mirrors;
