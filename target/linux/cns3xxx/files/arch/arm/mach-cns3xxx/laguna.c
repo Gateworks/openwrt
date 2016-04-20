c4f8202073042545d3f54735c63aa1efd51532fd
Tim Harvey <tharvey@gateworks.com>
2016-04-20 15:01:52 -0700

    cns3xxx: fix register for enabling SPI pins (r641)
    Also fixes a GPIO related regression from the upgrade to 3.10 from 3.8
    This patch affects all Laguna products
    
    Signed-off-by: Pushpal Sidhu <psidhu@gateworks.com>
    
    see: http://trac.gateworks.com/changeset/641

--- target/linux/cns3xxx/files/arch/arm/mach-cns3xxx/laguna.c
+++ target/linux/cns3xxx/files/arch/arm/mach-cns3xxx/laguna.c
@@ -809,7 +809,7 @@ static void __init laguna_init(void)
 
 	platform_device_register(&laguna_i2c_controller);
 
-	/* Set ext_int 0-3 drive strength to 21 mA */
+	/* Set I2C 0-3 drive strength to 21 mA */
 	reg = MISC_IO_PAD_DRIVE_STRENGTH_CTRL_B;
 	*reg |= 0x300;
 
@@ -817,9 +817,8 @@ static void __init laguna_init(void)
 	reg = MISC_GPIOB_PIN_ENABLE_REG;
 	*reg |= BIT(12) | BIT(13);
 
-	/* Enable MMC/SD pins */
-	reg = MISC_GPIOA_PIN_ENABLE_REG;
-	*reg |= 0xf80;
+	/* Enable SPI pins */
+	*reg |= BIT(7) | BIT(8) | BIT(9) | BIT(10) | BIT(11);
 
 	cns3xxx_pwr_clk_en(1 << PM_CLK_GATE_REG_OFFSET_SPI_PCM_I2C);
 	cns3xxx_pwr_power_up(1 << PM_CLK_GATE_REG_OFFSET_SPI_PCM_I2C);
